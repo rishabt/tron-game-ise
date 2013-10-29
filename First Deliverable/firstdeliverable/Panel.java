b4829f914bc323a73b5b9be4328ba120492df0ac
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-10-29 17:13:51 -0400

    Added the start button and copyright label

--- First Deliverable/firstdeliverable/Panel.java
+++ First Deliverable/firstdeliverable/Panel.java
@@ -8,6 +8,8 @@ import java.awt.event.*;
 public class Panel extends JPanel implements ActionListener{
 
 	private JLabel title;
+	private JLabel description;
+	private JButton start;
 	
 	public Panel(){
 		makeComponents();
@@ -16,13 +18,23 @@ public class Panel extends JPanel implements ActionListener{
 	
 	public void makeComponents(){
 		title = new JLabel("Prototype Demonstration I");
-		title.setFont(new Font("Times", Font.BOLD, 40));
+		title.setFont(new Font("Times", Font.BOLD, 37));
 		title.setBounds(310, 10, 510, 45);
+		
+		start = new JButton("Start Game");
+		start.setBounds(440, 166, 200, 60);
+		start.addActionListener(this);
+		
+		description = new JLabel("© Copyright of Group 7, ECSE 321, McGill University");
+		description.setFont(new Font("Times", Font.BOLD, 10));
+		description.setBounds(100, 500, 510, 45);
 	}
 	
 	public void makeLayout(){
 		setLayout(null);
 		add(title);
+		add(start);
+		add(description);
 	}
 	
 	@Override
