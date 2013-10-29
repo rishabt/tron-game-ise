875e7b76ca2cd5485a4d04c94103168da7dc3b05
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-10-29 15:11:35 -0400

    Added the skeleton code for the JPanel needed for the GUI for the First Deliverable

---
+++ First Deliverable/firstdeliverable/Panel.java
@@ -0,0 +1,27 @@
+package firstdeliverable;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Panel extends JPanel implements ActionListener{
+
+	public Panel(){
+		
+	}
+	
+	public void makeComponents(){
+		
+	}
+	
+	public void makeLayout(){
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
