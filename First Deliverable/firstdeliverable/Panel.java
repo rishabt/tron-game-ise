7c13f95847bcb15a337c5aa3ffda9d0ea6f7a70e
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-10-29 15:35:35 -0400

    Added the title label

--- First Deliverable/firstdeliverable/Panel.java
+++ First Deliverable/firstdeliverable/Panel.java
@@ -1,21 +1,28 @@
 package firstdeliverable;
 
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.*;
 
 public class Panel extends JPanel implements ActionListener{
 
+	private JLabel title;
+	
 	public Panel(){
-		
+		makeComponents();
+		makeLayout();
 	}
 	
 	public void makeComponents(){
-		
+		title = new JLabel("Prototype Demonstration I");
+		title.setFont(new Font("Times", Font.BOLD, 40));
+		title.setBounds(310, 10, 510, 45);
 	}
 	
 	public void makeLayout(){
-		
+		setLayout(null);
+		add(title);
 	}
 	
 	@Override
