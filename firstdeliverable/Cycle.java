13a9248fa10e3120d054ec54444565f04bd3cd89
GeoffreyLong <GLong14@GMail.com>
2013-11-03 16:24:26 -0500

    reset the folder

---
+++ firstdeliverable/Cycle.java
@@ -0,0 +1,48 @@
+/**
+ * @author Rishabh Tandon
+ */
+
+package firstdeliverable;
+
+public class Cycle {
+	private int xPos;
+	private int yPos;
+	private int curHeading;
+	private int playerNum;
+	//0-> left
+	//1 -> right
+	//2 -> up
+	//3 -> down
+	public boolean isAlive;
+	
+	
+	public Cycle(int xPos, int yPos, int curHeading, int playerNum, boolean isAlive){
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.curHeading = curHeading;
+		this.playerNum = playerNum;
+		this.isAlive = isAlive;
+	}
+	
+	public void setXPos(int i){
+		xPos = i;
+	}
+	public int getXPos(){
+		return xPos;
+	}
+	public void setYPos(int i){
+		yPos = i;
+	}
+	public int getYPos(){
+		return yPos;
+	}
+	public void setCurHeading(int i){
+		curHeading = i;
+	}
+	public int getCurHeading(){
+		return curHeading;
+	}
+	public int getPlayerNum(){
+		return playerNum;
+	}
+}
---
+++ firstdeliverable/Main.java
@@ -0,0 +1,17 @@
+/**
+ * @author Rishabh Tandon
+ */
+
+package firstdeliverable;
+
+import java.awt.Toolkit;
+
+import javax.swing.*;
+
+public class Main{
+	public static void main(String[] args){ 
+		Panel panel = new Panel();
+		Frame frame = new Frame();
+		Frame.addPanel(panel);
+	}
+}
