099da72e19efcd15b736d4009aff9702149ac0df
Rishabh Tandon <rishabh.tandon@mail.mcgill.ca>
2013-11-16 21:36:23 -0500

    Updated some minor things

--- SecondDeliverable/Main2.java
+++ SecondDeliverable/Main2.java
@@ -1,10 +1,13 @@
+package deliverable2;
 
 import javax.swing.*;
 
 public class Main2 extends JFrame{
 	
+	public static JFrame frame;
+	
 	public static void main(String[] args){
-		JFrame frame = new JFrame();
+		frame = new JFrame();
 		LoginGUI panel = new LoginGUI();
 		frame.add(panel);
 		frame.setBounds(0, 0, 600, 600);
